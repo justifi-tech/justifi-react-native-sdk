862126d16ea5365832d9ee07ed2c52fbe7fd7ba8
Natalia Calle <ncalle@xseed.com.uy>
2023-04-05 18:10:41 -0300

    fix native project names

--- ios/JustifiTests/JustifiTests.m
+++ ios/JustifiTests/JustifiTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface JustifyTests : XCTestCase
+@interface JustifiTests : XCTestCase
 
 @end
 
-@implementation JustifyTests
+@implementation JustifiTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

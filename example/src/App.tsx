74f9b17c12c08e1b141c9851911acfcf9d972a0f
Ignacio Contreres <icontreres@xseed.com.uy>
2023-04-08 19:28:01 -0300

    added bind from sdk to example js, to show sdk code in example node modules. Also added some dummy code on react native side to show a menu with BankAccountForm and CardForm

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,45 @@
+import { useState } from 'react';
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'justifi-react-native-sdk';
+import { StyleSheet, View, Button, Text } from 'react-native';
+import { BankAccountForm } from 'justifi-react-native-sdk';
+import { CardForm } from 'justifi-react-native-sdk';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [view, setView] = useState('menu');
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const renderContent = () => {
+    switch (view) {
+      case 'bankAccount':
+        return (
+          <>
+            <Text>Bank Account Form</Text>
+            <BankAccountForm />
+            <Button title="Back to Menu" onPress={() => setView('menu')} />
+          </>
+        );
+      case 'card':
+        return (
+          <>
+            <Text>Card Form</Text>
+            <CardForm />
+            <Button title="Back to Menu" onPress={() => setView('menu')} />
+          </>
+        );
+      default:
+        return (
+          <>
+            <Button
+              title="Bank Account Form"
+              onPress={() => setView('bankAccount')}
+            />
+            <Button title="Card Form" onPress={() => setView('card')} />
+          </>
+        );
+    }
+  };
 
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
+  return <View style={styles.container}>{renderContent()}</View>;
 }
 
 const styles = StyleSheet.create({

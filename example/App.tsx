015a4db4137c9a180c3c665288a0abd6547de873
Ignacio Contreres <icontreres@xseed.com.uy>
2023-04-10 19:22:57 -0300

    remaked example project without expo, added ios advances and menu on app tsx

---
+++ example/App.tsx
@@ -0,0 +1,58 @@
+import {BankAccountForm} from 'justifi-react-native-sdk';
+import {useState} from 'react';
+import * as React from 'react';
+
+import {StyleSheet, View, Button, Text} from 'react-native';
+
+export default function App() {
+  const [view, setView] = useState('menu');
+
+  const renderContent = () => {
+    switch (view) {
+      case 'bankAccount':
+        return (
+          <View style={styles.box}>
+            <Text>Bank Account Form</Text>
+            {/* <BankAccountForm
+              style={{height: 200, width: '100%', backgroundColor: 'red'}}
+            /> */}
+            <Button title="Back to Menu" onPress={() => setView('menu')} />
+          </View>
+        );
+      case 'card':
+        return (
+          <View style={styles.box}>
+            <Text>Card Form</Text>
+            {/* <CardForm /> */}
+            <Button title="Back to Menu" onPress={() => setView('menu')} />
+          </View>
+        );
+      default:
+        return (
+          <>
+            <Button
+              title="Bank Account Form"
+              onPress={() => setView('bankAccount')}
+            />
+            <Button title="Card Form" onPress={() => setView('card')} />
+          </>
+        );
+    }
+  };
+
+  return <View style={styles.container}>{renderContent()}</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+});

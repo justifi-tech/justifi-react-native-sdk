2d981f4e2bc579499fe00dc8b5a0b261d6860b12
Ignacio Contreres <icontreres@xseed.com.uy>
2023-04-25 19:49:50 -0300

    removed duplicated multiply

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,3 @@ export { CardForm } from './components/CardForm';
 export { validate, tokenize } from './utils/bankAccountFormFunctions';
 
 export * from './types/index';
-
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}

26207ce66cfafb0579b79906aedadffa7af9b788
Ignacio Contreres <icontreres@xseed.com.uy>
2023-04-25 14:58:12 -0300

    commented styles overrides

--- example/App.tsx
+++ example/App.tsx
@@ -17,50 +17,50 @@ export default function App() {
   //   console.log('Tokenized data:', tokenizedData);
   // };
 
-  const layout = {
-    padding: 0, //working
-    //TODO: ask about this ones to confirm with the Justifi team
-    // formControlSpacingHorizontal: 8,
-    // formControlSpacingVertical: 16,
-  };
+  // const layout = {
+  //   padding: 0, //working
+  //   //TODO: ask about this ones to confirm with the Justifi team
+  //   // formControlSpacingHorizontal: 8,
+  //   // formControlSpacingVertical: 16,
+  // };
 
-  const formLabel = {
-    fontWeight: '700', //working
-    fontFamily: 'sans-serif', //working
-    margin: {bottom: 5}, //working
-  };
+  // const formLabel = {
+  //   fontWeight: '700', //working
+  //   fontFamily: 'sans-serif', //working
+  //   margin: {bottom: 5}, //working
+  // };
 
-  const formControl = {
-    backgroundColor: '#EEEEF5', //working
-    borderColor: '#000015', //working
-    borderColorFocus: '#fccc32', //working
-    borderColorError: '#C12727', //TODO on api call validations
-    borderWidth: 1, //working
-    borderBottomWidth: 1,
-    borderRadius: {topLeft: 8, topRight: 4, bottomLeft: 0, bottomRight: 0}, //working
-    borderStyle: 'solid',
-    boxShadowErrorFocus: 'none', //TODO ask if this are neccesary, boxShadow does not exist on mobile, on IOS something similar could be elevation
-    boxShadowFocus: 'none', //TODO ask if this are neccesary, boxShadow does not exist on mobile, on IOS something similar could be elevation
-    color: '#212529', //working
-    fontSize: 16, //TODO ask if this increments the input height
-    fontWeight: '400', //working
-    lineHeight: 2, //TODO is this the input height?
-    margin: 0,
-    padding: {top: 8, bottom: 8, left: 14, right: 14},
-  };
+  // const formControl = {
+  //   backgroundColor: '#EEEEF5', //working
+  //   borderColor: '#000015', //working
+  //   borderColorFocus: '#fccc32', //working
+  //   borderColorError: '#C12727', //TODO on api call validations
+  //   borderWidth: 1, //working
+  //   borderBottomWidth: 1,
+  //   borderRadius: {topLeft: 8, topRight: 4, bottomLeft: 0, bottomRight: 0}, //working
+  //   borderStyle: 'solid',
+  //   boxShadowErrorFocus: 'none', //TODO ask if this are neccesary, boxShadow does not exist on mobile, on IOS something similar could be elevation
+  //   boxShadowFocus: 'none', //TODO ask if this are neccesary, boxShadow does not exist on mobile, on IOS something similar could be elevation
+  //   color: '#212529', //working
+  //   fontSize: 16, //TODO ask if this increments the input height
+  //   fontWeight: '400', //working
+  //   lineHeight: 2, //TODO is this the input height?
+  //   margin: 0,
+  //   padding: {top: 8, bottom: 8, left: 14, right: 14},
+  // };
 
-  const errorMessage = {
-    color: '#C12727', //working
-    margin: {top: 4, bottom: 0, left: 0, right: 0},
-    fontSize: 12, //working
-  };
+  // const errorMessage = {
+  //   color: '#C12727', //working
+  //   margin: {top: 4, bottom: 0, left: 0, right: 0},
+  //   fontSize: 12, //working
+  // };
 
-  const styleOverrides = {
-    layout: layout,
-    formLabel: formLabel,
-    formControl: formControl,
-    errorMessage: errorMessage,
-  };
+  // const styleOverrides = {
+  //   layout: layout,
+  //   formLabel: formLabel,
+  //   formControl: formControl,
+  //   errorMessage: errorMessage,
+  // };
 
   const renderContent = () => {
     switch (view) {

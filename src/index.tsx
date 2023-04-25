bc6ef795f9ff80e6fc647d4119c56a833dec5ef5
Ignacio Contreres <icontreres@xseed.com.uy>
2023-04-25 14:28:34 -0300

    added multiply function to pass test

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,7 @@ export { BankAccountForm } from './components/BankAccountForm';
 export { validate, tokenize } from './utils/bankAccountFormFunctions';
 
 export * from './types/index';
+
+export function multiply(a: number, b: number): Promise<number> {
+  return Promise.resolve(a * b);
+}

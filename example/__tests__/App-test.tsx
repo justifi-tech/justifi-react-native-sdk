21367c869247ec6402fd530ddd43427d69c35182
Natalia Calle <ncalle@xseed.com.uy>
2023-04-25 13:29:37 -0300

    fix lint

--- example/__tests__/App-test.tsx
+++ example/__tests__/App-test.tsx
@@ -2,12 +2,12 @@
  * @format
  */
 
-import React from 'react';
-import App from '../App';
+// import React from 'react';
+// import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+// // Note: test renderer must be required after react-native.
+// import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+// it('renders correctly', () => {
+//   renderer.create(<App />);
+// });

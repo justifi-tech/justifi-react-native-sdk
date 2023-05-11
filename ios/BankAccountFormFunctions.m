20348daecd67360438403e833d73f03c995effc6
Ignacio Contreres <icontreres@xseed.com.uy>
2023-05-11 20:51:04 -0300

    removed bank account form functions

--- ios/BankAccountFormFunctions.m
+++
@@ -1,8 +0,0 @@
-#import "React/RCTBridgeModule.h"
-
-@interface RCT_EXTERN_MODULE(BankAccountFormFunctions, NSObject)
-
-RCT_EXTERN_METHOD(validate:(RCTResponseSenderBlock)callback)
-RCT_EXTERN_METHOD(tokenize:(RCTResponseSenderBlock)callback)
-
-@end

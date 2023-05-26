8e242158406cbd492138b949847c46e9c748c85e
A-Fanton <andrealuciacf@gmail.com>
2023-05-26 14:17:14 -0300

    performance test

--- src/components/BankAccountForm.test.tsx
+++ src/components/BankAccountForm.test.tsx
@@ -36,4 +36,24 @@ describe('BankAccountForm', () => {
     const bankAccountForm = getByTestId('bank-account-form');
     expect(bankAccountForm).toBeDefined();
   });
+
+  it('should render within a specific time limit', () => {
+    // Set the desired time limit for rendering
+    const timeLimit = 100; // in milliseconds
+    // Start the timer
+    const startTime = performance.now();
+    // Render the component
+    render(
+      <BankAccountForm
+        open={true}
+        onClose={onCloseMock}
+        onSubmit={onSubmitMock}
+      />
+    );
+    // Calculate the rendering time
+    const renderingTime = performance.now() - startTime;
+    // Check if the rendering time is within the time limit
+    expect(renderingTime).toBeLessThan(timeLimit);
+  });
+
 });
